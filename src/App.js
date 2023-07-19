6aaaf412a0ec7ad388bd3f4ea4cf6a29347bc1b6
anamamani <anamamaniparo@gmail.com>
2023-07-18 18:20:31 -0600

    se agrego 3 componentes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import C01componente from './components/C01componente';
 import C02contador from './components/C02contador';
 import C03doblecontador from './components/C03doblecontador';
+import C04variable from './components/C04variable';
+import C05operadorTernario from './components/C05operadorTernario';
 
 function App() {
   return (
@@ -13,6 +15,13 @@ function App() {
       <C02contador />
       <h3>Tercer componente</h3>
       <C03doblecontador />
+      <h3>cuarto componente </h3>
+      <C04variable xVariable="hola mundo...." />
+      <h1>Quinto componente</h1>
+      <C05operadorTernario />
+      <h1>Sexto  componente</h1>
+
+     
     </div>
   );
 }
